f967325003ccf48a844241eae7f1c95915700572
JasmineBabaYara <jassyprincess1@gmail.com>
2021-12-01 17:59:31 -0800

    commit before revert to previous commit

--- screens/welcomeScreen.js
+++ screens/welcomeScreen.js
@@ -1,14 +1,47 @@
-import React from "react";
-import { View, Text, Button } from "react-native";
+import React, { useEffect } from "react";
+import {
+  View,
+  Text,
+  ImageBackground,
+  TouchableOpacity,
+  Animated,
+} from "react-native";
 
 // Welcome Screen Styles
 import styles from "../styles/welcomeScreen.style";
 
 export default function WelcomeScreen({ setIsLoggedIn }) {
+  const animated = new Animated.Value(255);
+  const duration = 5000;
+
+  useEffect(() => {
+
+  })
+
+
   return (
-    <View style={styles.container}>
-      <Text>Welcome Screen</Text>
-      <Button title="SIGN IN TO HOMEPAGE" onPress={() => setIsLoggedIn(true)} />
+    <View style={styles.welcomecontainer}>
+      <View>
+        <ImageBackground
+          source={require("../assets/img/bg.png")}
+          style={styles.image}
+        />
+      </View>
+      <Animated.View style={styles.welcome}>
+        <View>
+          <Text style={styles.header}>Welcome</Text>
+          <Text style={styles.message}>
+            Find the appropriate job for your skills{"\n"}
+            Leverage your skills and knowledge in the job market{"\n"}
+            Join us for the experience of a career lifetime
+          </Text>
+        </View>
+        <TouchableOpacity>
+          <Text style={styles.signin} onPress={() => setIsLoggedIn(true)}>
+            Sign In With Google
+          </Text>
+        </TouchableOpacity>
+      </Animated.View>
     </View>
   );
 }
--- styles/welcomeScreen.style.js
+++ styles/welcomeScreen.style.js
@@ -1,10 +1,64 @@
 import { StyleSheet } from "react-native";
 
+
+
 const styles = StyleSheet.create({
-  container: {
+  welcomecontainer: {
+    backgroundColor: "#fff",
+    height: "100%",
+    width: "100%",
     flex: 1,
     alignItems: "center",
     justifyContent: "center",
+  },
+
+  image:{
+    width: "100%",
+    height:"100%",
+  },
+
+  welcome: {
+    overflow: "hidden",
+    borderWidth: 1,
+    borderColor: "rgba(161, 170, 173, 0.136)",
+    borderRadius: 30,
+    paddingTop: 40,
+    paddingLeft:10,
+    paddingBottom:20,
+    paddingRight:20,
+    backgroundColor: "rgba(255, 255, 255, 0.25)",
+    shadowOpacity: 10,
+    shadowRadius: 10,
+    shadowOffset:{
+      width:0,
+      height: 2,
+    },
+    shadowColor: "rgba(31, 38, 135, 0.37)",
+    elevation:3,
+  },
+
+  header: {
+    fontSize: 30,
+    paddingBottom: 0,
+  },
+
+  message: {
+    fontSize: 16,
+    marginBottom: 0,
+  },
+
+  signin: {
+    height: 60,
+    padding: 20,
+    outline: "none",
+    borderRadius: 28,
+    border: "none",
+    fontSize: 16,
+    fontWeight: "bold",
+    margin: 30,
+    backgroundColor: "#000",
+    color: "#fff",
+    textAlign: "center",
   },
 });
 
